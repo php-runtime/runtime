4770ca8abfd8985854d154904e59dd70da1c41a6
Nyholm <tobias.nyholm@gmail.com>
2021-04-27 20:10:17 +0200

    cs

--- src/google-cloud/tests/runtime/ExecutionStopped.php
+++ src/google-cloud/tests/runtime/ExecutionStopped.php
@@ -1,10 +1,10 @@
 <?php
 
-
 namespace Runtime\GoogleCloud\Tests;
 
 /**
- * An exception to be used in tests to make sure we called `exit()`;
+ * An exception to be used in tests to make sure we called `exit()`;.
+ *
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  */
 class ExecutionStopped extends \RuntimeException
