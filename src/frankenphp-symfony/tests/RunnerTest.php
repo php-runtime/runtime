90f0d768d2356901da4e4c89c727301face20d10
Maximilian <maximilian.reichel@backbrain.io>
2024-01-19 12:03:40 +0100

    feat(frankenphp-symfony): add kernel reboot strategy

--- src/frankenphp-symfony/tests/RunnerTest.php
+++ src/frankenphp-symfony/tests/RunnerTest.php
@@ -11,9 +11,10 @@ use Runtime\FrankenPhpSymfony\Runner;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\RebootableInterface;
 use Symfony\Component\HttpKernel\TerminableInterface;
 
-interface TestAppInterface extends HttpKernelInterface, TerminableInterface
+interface TestAppInterface extends HttpKernelInterface, TerminableInterface, RebootableInterface
 {
 }
 
@@ -34,10 +35,27 @@ class RunnerTest extends TestCase
                 return new Response();
             });
         $application->expects($this->once())->method('terminate');
+        $application->expects($this->never())->method('reboot');
 
         $_SERVER['FOO'] = 'bar';
 
-        $runner = new Runner($application, 500);
+        $runner = new Runner($application, 500, 'never');
+        $this->assertSame(0, $runner->run());
+    }
+
+    public function testRebootAlways(): void
+    {
+        $application = $this->createMock(TestAppInterface::class);
+        $application
+            ->expects($this->once())
+            ->method('handle')
+            ->willReturnCallback(function (Request $request, int $type = HttpKernelInterface::MAIN_REQUEST, bool $catch = true): Response {
+                return new Response();
+            });
+        $application->expects($this->once())->method('terminate');
+        $application->expects($this->once())->method('reboot');
+
+        $runner = new Runner($application, 500, 'always');
         $this->assertSame(0, $runner->run());
     }
 }
